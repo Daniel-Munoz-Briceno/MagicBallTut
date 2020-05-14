d86e5d27109f671ec38ec5faa60fcc17374aea28
Daniel-Munoz-Briceno <63081866+Daniel-Munoz-Briceno@users.noreply.github.com>
2020-05-14 12:26:04 -0400

    Fix and Style: update v1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.myapplication;
 
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onClick(View v) {
+
     }
 }
